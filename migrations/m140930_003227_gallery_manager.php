fc582885a1db9cea0dd3811db4bf53c644903794
Elgorm <8758619+Elgorm@users.noreply.github.com>
2020-12-14 09:31:30 +0500

    Update m140930_003227_gallery_manager.php

--- migrations/m140930_003227_gallery_manager.php
+++ migrations/m140930_003227_gallery_manager.php
@@ -14,12 +14,12 @@ class m140930_003227_gallery_manager extends Migration
         $this->createTable(
             $this->tableName,
             array(
-                'id' => Schema::TYPE_PK,
-                'type' => Schema::TYPE_STRING,
-                'ownerId' => Schema::TYPE_STRING . ' NOT NULL',
-                'rank' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
-                'name' => Schema::TYPE_STRING,
-                'description' => Schema::TYPE_TEXT
+                'id' => $this->primaryKey(),
+                'type' => $this->string(),
+                'ownerId' => $this->string()->notNull(),
+                'rank' => $this->integer()->notNull()->defaultValue(0),
+                'name' => $this->string(),
+                'description' => $this->string()
             )
         );
     }
